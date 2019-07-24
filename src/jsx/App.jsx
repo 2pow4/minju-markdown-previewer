9e719e4e6811586a638b69862fc4fc9a2a8201f3
kmju1997 <kmju1997@gmail.com>
2019-07-24 23:32:37 +0900

    initial commit

---
+++ src/jsx/App.jsx
@@ -0,0 +1,13 @@
+const React = require('react')
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello, React! ft. Babel and Webpack
+      </div>
+    )
+  }
+}
+
+module.exports = App
